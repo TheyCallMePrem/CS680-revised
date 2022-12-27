79d1eb46e29c7bab4cf8b4109c45ac62c5c19316
Prem D <prem.desai0008@gmail.com>
2022-12-26 20:40:04 -0500

    Create Directory.java

---
+++ hw10/src/edu/umb/cs680/hw10/fs/Directory.java
@@ -0,0 +1,90 @@
+package edu.umb.cs680.hw10.fs;
+
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+
+public class Directory extends FSElement{
+    Directory parent;
+
+    LinkedList<FSElement> FSlist = new LinkedList<FSElement>();
+    LinkedList<Directory> DIRList = new LinkedList<Directory>();
+    LinkedList<File> Filelist = new LinkedList<File>();
+    LinkedList<Link> ProxyList = new LinkedList<Link>();
+
+    public Directory(Directory parent, String name, int size,LocalDateTime ldt){
+        super( parent, name, size, ldt);
+        this.parent=parent;
+    }
+
+    public LinkedList<FSElement> getChildren(){
+        return this.FSlist;
+    }
+
+    public void appendChild(FSElement child){
+        FSlist.add(child);
+
+        if(child.isDirectory()){
+            DIRList.add((Directory) child);
+        }
+        else{
+            if(child.isProxy()){
+                ProxyList.add((Link) child);
+            }
+            else{
+                Filelist.add((File) child);
+            }
+            
+        }
+    }
+
+    public int countChildren(){
+        return FSlist.size();
+    }
+
+    public LinkedList<Directory> getSubDirectories(){
+        
+        
+        return this.DIRList;
+    }
+
+    public LinkedList<File> getFiles(){
+        return this.Filelist;
+    }
+
+    public int getTotalSize(){
+        int totalSize=0;
+
+        for (Directory childDirectory : DIRList) {
+            totalSize+= childDirectory.getTotalSize();
+        }
+
+        for(File f : Filelist){
+            totalSize+= f.getSize();
+        }
+        return totalSize;
+    }
+
+    public Directory getParentDirectory(){
+        return this.parent;
+    }
+
+    public LinkedList<Link> getProxies(){
+        return this.ProxyList;
+    }
+
+    @Override
+    public boolean isDirectory() {
+        return true;
+    }
+
+    @Override
+    public boolean isProxy() {
+        return  false;
+    }
+    @Override
+    public void accept(FSVisitor v){
+        for(FSElement e: FSlist){
+            e.accept(v); 
+        }
+    }
+}
