a7cd110c2cb4012e8caf540cf8ef992398baf68f
Prem D <prem.desai0008@gmail.com>
2022-12-27 16:22:19 -0500

    Create CountingVisitor.java

---
+++ hw15/src/edu/umb/cs680/hw15/fs/util/CountingVisitor.java
@@ -0,0 +1,43 @@
+package edu.umb.cs680.hw15.fs.util;
+
+
+import edu.umb.cs680.hw15.fs.*;
+
+public class CountingVisitor implements FSVisitor{
+    
+
+    private int dirNum = 0;
+	private int fileNum = 0;
+	private int linkNum = 0;
+    @Override
+    public void visit(Link link) {
+        linkNum++;
+    }
+
+    @Override
+    public void visit(Directory dir) {
+        fileNum++;
+    }
+
+    @Override
+    public void visit(File file) {
+        dirNum++;
+    }
+
+    public int totalElementsVisited(){
+        return (linkNum + fileNum + dirNum);
+    }
+
+    public int getdirNum(){
+        return dirNum;
+    }
+
+    public int getfileNum(){
+        return fileNum;
+    }
+
+    public int getlinkNum(){
+        return linkNum;
+    }
+    
+}
